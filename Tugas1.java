b6849fd930f50f8e1cfab3b40fde024574e03f50
christinaa21 <89820242+christinaa21@users.noreply.github.com>
2022-06-02 11:12:02 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,17 @@
+public class Tugas1 {
+    public static void main(String args[]) {
+        //Buatlah aplikasi untuk menampilkan angka 1-50,
+        //ketika angka n habis dibagi 3 maka keluar tulisan
+        //"n habis dibagi 3", dan jika n habis dibagi 5
+        //maka keluar tulisan "n habis dibagi 5"
+        for (int angka = 1; angka <51; angka++) {
+            System.out.println(angka);
+            if (angka % 3 == 0) {
+                System.out.println(angka + " habis dibagi 3");
+            }
+            else if (angka % 5 == 0) {
+                System.out.println(angka + " habis dibagi 5");
+            }
+        }
+    }
+}
